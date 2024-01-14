c9deb127f526fd2b177924f41d95cf1ac524c365
Jonas <117071036+sajonasJ@users.noreply.github.com>
2024-01-14 20:38:33 +1000

    Delete lab1_js directory

--- lab1_js/main2.js
+++
@@ -1,5 +0,0 @@
-var num1 = 30;
-var num2 = 20;
-var num3 = 50;
-var sum = "This is the sum of the numbers:".concat(num1 + num2 + num3);
-console.log(sum);
--- lab1_js/main2.ts
+++
@@ -1,8 +0,0 @@
-let num1: number=30;
-let num2: number=20;
-let num3: number=50;
-let sum:string=`This is the sum of the numbers:${num1+num2+num3}`;
-
-console.log(sum);
-// The mode determines which platform styles to use
-// ios for apple & md for android
